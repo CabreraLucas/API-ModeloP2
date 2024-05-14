25f821abbba6707986909a6dc15293de8d5f2e7d
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2024-05-14 18:10:06 +0000

    Autenticacao + HTTP

--- app/src/main/java/application/controller/AutenticacaoController.java
+++ app/src/main/java/application/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +17,20 @@ import application.service.TokenService;
 @RequestMapping("/auth")
 public class AutenticacaoController {
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private AuthenticationManager authManager;
 
     @Autowired
     private TokenService tokenService;
 
     @PostMapping
     public String login(@RequestBody Usuario usuario){
-        //return "Exemplo de JWT";
+        UsernamePasswordAuthenticationToken tk = new UsernamePasswordAuthenticationToken(
+            usuario.getNomeDeUsuario(),
+            usuario.getSenha()
+        );
+
+        Authentication authentication = authManager.authenticate(tk);
+
         return tokenService.generateToken(usuario);
     }
 }
