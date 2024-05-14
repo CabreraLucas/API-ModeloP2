e27e2e79d2a32ad93dcf83f80ceadea18eb021ff
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2024-05-14 17:36:11 +0000

    Autenticacao

--- app/src/main/java/application/controller/AutenticacaoController.java
+++ app/src/main/java/application/controller/AutenticacaoController.java
@@ -1,15 +1,27 @@
 package application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.model.Usuario;
+import application.service.TokenService;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
     public String login(@RequestBody Usuario usuario){
-        return "Exemplo de JWT";
+        //return "Exemplo de JWT";
+        return tokenService.generateToken(usuario);
     }
 }
